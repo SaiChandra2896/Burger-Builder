d56041faadf54aa7926e5232db3ad925e00458cf
SaiChandra2896 <saichandra2896@gmail.com>
2019-06-08 19:17:58 +0530

    added dynamic rendering to the burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,28 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  let transformedingredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      // console.log(igKey);
+      // console.log([...Array(props.ingredients[igKey])]);
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // console.log(i);
+        // console.log(_);
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  //console.log(transformedingredients);
+  if (transformedingredients.length === 0) {
+    transformedingredients = <p>Please start adding ingredients!!</p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
